317fb4602fee5938077c12efe807eb73ec3fafb1
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-08-26 22:06:19 +0100

    Setup other pages

--- website/urls.py
+++ website/urls.py
@@ -5,4 +5,8 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('test/', views.test, name='test'),
+    path('create/', views.create, name='create'),
+    path('user/<str:user>/recipe/<str:recipe>', views.recipe, name='recipe'),
+    path('user/<str:user>', views.profile, name='profile'),
+
 ]
--- website/views.py
+++ website/views.py
@@ -1,5 +1,13 @@
+from django.conf import settings
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+
+from .models import Recipe
+
+
+def is_author(user: settings.AUTH_USER_MODEL) -> bool:
+    """Check if user is an author."""
+    return user.groups.filter(name='Author').exists()
 
 
 # Create your views here.
@@ -10,4 +18,21 @@ def test(request: HttpRequest) -> HttpResponse:
 
 def home(request: HttpRequest) -> HttpResponse:
     """Website Homepage."""
-    return render(request, "website/home.html")
+    recipes = Recipe.objects.all().order_by("title")
+    print(f"{request.user.username} {'is' if is_author(request.user) else 'isnt'} an author.")
+    return render(request, "website/home.html", {"recipes": recipes, "author": is_author(request.user)})
+
+
+def create(request: HttpRequest) -> HttpResponse:
+    """Recipe Creation page."""
+    return render(request, "website/create.html")
+
+
+def profile(request: HttpRequest, user: str) -> HttpResponse:
+    """User's profile page."""
+    return render(request, "website/profile.html")
+
+
+def recipe(request: HttpRequest, user: str, title: str) -> HttpResponse:
+    """Recipe page."""
+    return render(request, "website/recipe.html")
