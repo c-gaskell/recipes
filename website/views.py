d63bbbd433464359e215cc64cf629a843db7be01
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-08-26 01:52:06 +0100

    Add homepage

--- website/views.py
+++ website/views.py
@@ -3,4 +3,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def test(request):
-    return render(request, 'website/test.html')
+    return render(request, "website/test.html")
+
+
+def home(request):
+    return render(request, "website/home.html")
