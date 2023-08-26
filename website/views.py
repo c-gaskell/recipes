fe32adc313d96495b75db408fc61d05166f99f67
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-08-26 13:45:38 +0100

    Linting

--- website/views.py
+++ website/views.py
@@ -1,10 +1,13 @@
+from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
 
 # Create your views here.
-def test(request):
+def test(request: HttpRequest) -> HttpResponse:
+    """Basic Test Page. Used for checking templates, styles etc."""
     return render(request, "website/test.html")
 
 
-def home(request):
+def home(request: HttpRequest) -> HttpResponse:
+    """Website Homepage."""
     return render(request, "website/home.html")
