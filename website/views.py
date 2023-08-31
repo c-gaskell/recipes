c046ccb0c8599f3d68d8629f82f687052e3e7db1
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-08-31 11:47:08 +0100

    Add BaseView and convert HomeView to subclass

--- website/views.py
+++ website/views.py
@@ -1,4 +1,6 @@
-from django.conf import settings
+from typing import Any, Dict
+
+from django.contrib.auth.models import User
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 from django.views import View
@@ -6,7 +8,32 @@ from django.views import View
 from .models import Recipe
 
 
-def is_author(user: settings.AUTH_USER_MODEL) -> bool:
+class BaseView(View):
+    """Parent class for pages using the `website/base.html` template."""
+
+    template = "website/base.html"
+
+    def get_page_attrs(self, request: HttpRequest) -> Dict[str, Any]:
+        """Get attributes shown in the page template.
+
+        This method will provide attributes used by the base template.
+        If a subclass needs to provide its own attributes, it should also call the super function.
+        """
+        attrs = {}
+        attrs['author'] = is_author(request.user)
+        return attrs
+
+    def get(self, request: HttpRequest) -> HttpResponse:
+        """HTTP GET Method.
+
+        Generic method which will use get_page_attrs and self.template.
+        If a subclass only needs custom attributes, just override get_page_attrs and not this method.
+        """
+        attrs = self.get_page_attrs(request)
+        return render(request, self.template, attrs)
+
+
+def is_author(user: User) -> bool:
     """Check if user is an author."""
     return user.groups.filter(name='Author').exists()
 
@@ -17,14 +44,16 @@ def test(request: HttpRequest) -> HttpResponse:
     return render(request, "website/test.html")
 
 
-class HomeView(View):
+class HomeView(BaseView):
     """Homepage view."""
 
-    def get(self, request: HttpRequest) -> HttpResponse:
-        """HTTP GET method."""
-        recipes = Recipe.objects.all().order_by("title")
-        print(f"{request.user.username} {'is' if is_author(request.user) else 'isnt'} an author.")
-        return render(request, "website/home.html", {"recipes": recipes, "author": is_author(request.user)})
+    template = "website/home.html"
+
+    def get_page_attrs(self, request: HttpRequest) -> Dict[str, str]:
+        """Get recipies to display in page."""
+        attrs = super().get_page_attrs(request)
+        attrs['recipies'] = Recipe.objects.all().order_by("title")
+        return attrs
 
 
 def create(request: HttpRequest) -> HttpResponse:
