bb534040d969a6e53befc73117a25ad7c1eaae4d
Charlotte Gaskell <charlotte.gaskell.0@gmail.com>
2023-08-31 12:42:04 +0100

    Modify parameter names in recipe URL

--- website/urls.py
+++ website/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('test/', views.TestView.as_view(), name='test'),
     path('create/', views.CreateView.as_view(), name='create'),
-    path('user/<str:user>/recipe/<str:recipe>', views.RecipeView.as_view(), name='recipe'),
+    path('user/<str:user>/recipe/<str:title>', views.RecipeView.as_view(), name='recipe'),
     path('user/<str:user>', views.ProfileView.as_view(), name='profile'),
 
 ]
